621dcea4a6e724dca02fb6050f67f9c39569c7e5
Drakunal <drakunaldey1@gmail.com>
2021-08-06 12:14:46 +0530

    ticker symbol for yfinance declared

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import yfinance as yf
 import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+# it is in markup language
+
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
